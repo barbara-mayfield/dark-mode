55a6f7002223dcb0f40891877d269316fff41312
Barbara Mayfield <bmayfield1991@gmail.com>
2019-10-08 18:56:21 -0500

    Created stateful logic in useDarkMode and useLocalStorage, finished Step 2 & 3, MVP complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key, initialValue) => {
+    const [value, newValue] = useLocalStorage(key, initialValue)
+
+    useEffect(() => {
+        if (value === true) {
+            document.querySelector('body').classList.add('dark-mode')
+        } else {
+            document.querySelector('body').classList.remove('dark-mode')
+        }
+    }, [value])
+
+    return [value, newValue]
+}
